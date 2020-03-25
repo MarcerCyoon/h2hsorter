38bfa564e6c37ded0849e5da81e6b664c9aaaadd
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2020-03-25 20:09:23 +0900

    Cleaned up code a little

---
+++ sets.js
@@ -0,0 +1,23 @@
+var nbaPlayersSet = [
+  "LeBron James",
+  "James Harden",
+  "Giannis Antetokounmpo",
+  "Stephen Curry",
+  "Joel Embiid",
+  "Anthony Davis",
+  "Damian Lillard",
+  "Kawhi Leonard"
+];
+
+var nbaTeamsSet = [
+  "Atlanta Hawks",
+  "Boston Celtics",
+  "Brooklyn Nets",
+  "Charlotte Hornets",
+  "Chicago Bulls",
+  "Cleveland Cavaliers",
+  "Dallas Mavericks",
+  "Denver Nuggets",
+  "Detroit Pistons",
+  "Golden State Warriors"
+];
